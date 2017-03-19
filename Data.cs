f516760a2b52e0e0b513c6c381a314df37a88815
Mateusz Chodkowski <chotkos@gmail.com>
2017-03-19 20:19:22 +0100

    crucial folder structure changes

---
+++ Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Awesomium_in_WF
+{
+    public static class Data
+    {
+        public static string Password { get; set; }
+        public static string Login { get; set; }
+        public static int Likes { get; set; }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Awesomium_in_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginText = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.isActive = new System.Windows.Forms.Label();
+            this.likesCounter = new System.Windows.Forms.Label();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webControl1
+            // 
+            this.webControl1.Location = new System.Drawing.Point(9, 129);
+            this.webControl1.Size = new System.Drawing.Size(628, 338);
+            this.webControl1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.likesCounter);
+            this.groupBox2.Controls.Add(this.isActive);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.passwordBox);
+            this.groupBox2.Controls.Add(this.loginText);
+            this.groupBox2.Controls.Add(this.loginButton);
+            this.groupBox2.Location = new System.Drawing.Point(9, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(631, 111);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Użytkownik";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(374, 19);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(251, 48);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Start";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // loginText
+            // 
+            this.loginText.Location = new System.Drawing.Point(86, 21);
+            this.loginText.Name = "loginText";
+            this.loginText.Size = new System.Drawing.Size(271, 20);
+            this.loginText.TabIndex = 2;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(86, 47);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(271, 20);
+            this.passwordBox.TabIndex = 3;
+            this.passwordBox.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Hasło";
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Interval = 180000;
+            this.refreshTimer.Tick += new System.EventHandler(this.refreshTimer_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(517, 480);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mateusz Chodkowski 2016";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // isActive
+            // 
+            this.isActive.AutoSize = true;
+            this.isActive.Location = new System.Drawing.Point(9, 78);
+            this.isActive.Name = "isActive";
+            this.isActive.Size = new System.Drawing.Size(129, 13);
+            this.isActive.TabIndex = 6;
+            this.isActive.Text = "Aplikacja nie jest aktywna";
+            // 
+            // likesCounter
+            // 
+            this.likesCounter.AutoSize = true;
+            this.likesCounter.Location = new System.Drawing.Point(371, 78);
+            this.likesCounter.Name = "likesCounter";
+            this.likesCounter.Size = new System.Drawing.Size(253, 13);
+            this.likesCounter.TabIndex = 7;
+            this.likesCounter.Text = "Zaloguj się tylko gdy poniżej widać ikonkę aparatu :)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 502);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.groupBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(671, 541);
+            this.MinimumSize = new System.Drawing.Size(671, 541);
+            this.Name = "Form1";
+            this.Text = "DressCloud Bot 1.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Awesomium.Windows.Forms.WebControl webControl1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.TextBox loginText;
+        private System.Windows.Forms.Timer refreshTimer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label likesCounter;
+        private System.Windows.Forms.Label isActive;
+    }
+}
+
